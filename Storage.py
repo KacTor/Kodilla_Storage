d25404aae7a7b707e3cca913d98ad11d16c8230d
Kacper Torba <ka.toorba@gmail.com>
2023-01-13 16:53:59 +0100

    Modify sell option and add revenue option

--- Storage.py
+++ Storage.py
@@ -1,3 +1,4 @@
+from fileinput import close
 from tabulate import tabulate
 import logging
 
@@ -12,15 +13,22 @@ items = {'Name': ['Petrol', 'Sand', 'Rope'],
          'Unit Price (PLN)': [7, 15, 4]
          }
 
+soldItems = {'Name': [],
+             'Quantity': [],
+             'Unit': [],
+             'Unit Price (PLN)': []
+             }
+
 
 def select_operation(listOfOperation):
     while True:
-        print('\n')
+
         for index, operation in enumerate(listOfOperation):
             print(index, operation)
 
         try:
             index = int(input("\nWhat would you like to do ?"))
+            print('\n')
 
             if index <= len(listOfOperation) - 1:
                 return index
@@ -34,8 +42,46 @@ def select_operation(listOfOperation):
             continue
 
 
-def get_items():
-    print(tabulate(items, headers='keys', tablefmt='fancy_grid', missingval='N/A'))
+def add_to_soldItems(name, quantity, unit, unitPrice):
+    listOfName = soldItems['Name']
+    index = listOfName.index(name)
+
+    if name not in listOfName:
+        soldItems['Name'].append(name)
+        soldItems['Quantity'].append(quantity)
+        soldItems['Unit'].append(unit)
+        soldItems['Unit Price (PLN)'].append(unitPrice)
+    else:
+        soldItems['Quantity'][index] += quantity
+
+
+def get_items(listOfItems):
+    print(tabulate(listOfItems, headers='keys',
+          tablefmt='fancy_grid', missingval='N/A'))
+
+
+def get_costs_income(listOfItems):
+    '''if len(listOfItems['Quantity']) != len(listOfItems['Unit Price (PLN)']):
+        logging.debug('Wrong table, Quantity != Unit Price')
+        print('Check the table, different amount of values')
+
+    else:'''
+    sumOfIndividualProducts = [listOfItems['Quantity'][i] * listOfItems['Unit Price (PLN)'][i]
+                               for i in range(len(listOfItems['Name']))
+                               ]
+    finalSum = sum(sumOfIndividualProducts)
+    return round(finalSum, 2)
+
+
+def show_revenue(costs, income):
+    finalCosts = get_costs_income(costs)
+    finalIncome = get_costs_income(income)
+    revenue = finalIncome - finalCosts
+
+    print(f'''Income: {finalIncome}
+Costs: {finalCosts}
+---------------
+Revenue: {revenue} PLN''')
 
 
 def add_item():
@@ -58,7 +104,7 @@ def add_item():
                 items[dict].append(listOfInput[i])
                 i += 1
             print('Successfully added to storage. Current status:')
-            get_items()
+            get_items(items)
             break
         else:
             print('Item has not been added.')
@@ -75,10 +121,12 @@ def sell_item():
 
             if name in listOfName:
                 quantity = int(input('The quantity you want to sell:'))
+                logging.debug('Wrong type of input')
                 maxQuantity = items['Quantity'][index]
                 unit = items['Unit'][index]
+                unitPrice = items['Unit Price (PLN)'][index]
 
-                if items['Quantity'][index] - quantity < 0:
+                if int(items['Quantity'][index]) - quantity < 0:
                     logging.info(f'Not enough {name}')
                     answer = input(
                         f'Are you want to sell maximum of {name} - {maxQuantity}? (y/n) ')
@@ -88,35 +136,39 @@ def sell_item():
                         print(
                             f'Successfully sold {zeroQuantity} {unit} of {name}')
                         items['Quantity'][index] = 0
-                        get_items()
+                        get_items(items)
+                        add_to_soldItems(name, quantity, unit, unitPrice)
                         break
 
                 else:
                     items['Quantity'][index] -= quantity
                     print(
                         f'Successfully sold {quantity} {unit} of {name}')
-                    get_items()
+                    get_items(items)
+                    add_to_soldItems(name, quantity, unit, unitPrice)
+
                 break
 
         except ValueError:
-            print('Wrong name of product')
             logging.debug('Wrong name of product')
             break
 
 
-operations = ['Exit', 'Show your storage', 'Add new item', 'Sell item']
+operations = ['Exit', 'Show your storage',
+              'Add new item', 'Sell item', 'Show revenue']
 
 
 if __name__ == "__main__":
 
     while True:
+        print('\n')
         index = select_operation(operations)
 
         if index == 0:
             print("Exiting...Bye!")
             exit(1)
         elif index == 1:
-            get_items()
+            get_items(items)
 
         elif index == 2:
             print('Adding to storage...')
@@ -125,3 +177,7 @@ if __name__ == "__main__":
         elif index == 3:
             print('Selling item...')
             sell_item()
+
+        elif index == 4:
+            print('Revenue breakdown (PLN)...')
+            show_revenue(items, soldItems)
